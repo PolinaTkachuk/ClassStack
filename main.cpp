1c397b4b827cca2a863f9dd920c40fc4ac1b063c
Pilina <tkachuk_polina@bk.ru>
2022-03-24 15:51:01 +0700

    1 part lab tested commented and completed

--- main.cpp
+++ main.cpp
@@ -5,40 +5,39 @@ using namespace std;
 
 int main()
 {
+      // тестирование часть№1 лаба№1
+        Stack <int> S; //создание пустого стека
+        try
+        {
+           S.Pop();//проверка работы исключения при пустом стека
+         }
+        catch (const EStackException& e )//ловим исключение из throw в функции Pop
+        {
+            cout << e.what()<<endl; //вывод сообщения
+        }
+
+        Stack <int> S1; //создание стека
+        cout << "Использование Push" << endl;
+        for(int i=5;i>0;i--) //заполнение его 5ю значениями int
+        S1.Push(i); //проверка работы функции добавления
+        S1.print(); //вывод на консоль
+
+        cout << endl<<"Удаляем 1элемент, использование Pop" << endl;
+        S1.Pop();
+        S1.print();//вывод на консоль
+
+        cout<<endl<<"размер стека S1="<<S1.Size()<<endl;
+        S1.Clear();
+        cout << "cтек S1 удален";
+        S1.print();
+        cout << endl << "размер удаленного стека S1=" << S1.Size() << endl;
+
+  // тестирование часть№2 лаба№1
     PersonKeeper& keeper = PersonKeeper::Instance();
     string s = "D:\\lab\\ClassStack\\file.txt";
     keeper.ReadPersons(s);
 
-    Stack <int> S1;
-    Stack <int> S2(5);
 
-    if (S1.Empty()) cout << "S1 is Empty"<<endl;
-    else cout << "S1 is not empty";
-
-    if(S2.Empty()) cout<<"S2 is Empty";
-    else cout<<"S2 is not empty"<<endl;
-
-    //исключение при пустом стеке
-    //S1.Pop();
-
-       cout << "enter 6 integer numbers: " << endl; // вводим 6 чисел
-       for (int i = 0; i != 6;i++) {
-        int a;
-        cin >> a;
-
-        S1.Push(a);  // добавляем введенные числа
-      }
-       S1.print();
-
-       cout << endl<<"S2:" << endl;
-       S2.print();
-       cout << endl<<"delete element from S2 " << endl;
-       S2.Pop();
-       S2.print();
-       cout <<endl<< "Size S2=";
-       cout<<S2.Size()<<endl;
-
-       cout << endl<<"Size S1=";
-       cout << S1.Size()<<endl;
 
 }
+
